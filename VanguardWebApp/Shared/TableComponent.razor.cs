cd23508e38aa4737b20cd161e65796fbd066eb51
Nikolaj Mørup <nikolajmorup@gmail.com>
2020-03-22 18:46:28 +0100

    TableComponent added

---
+++ VanguardWebApp/Shared/TableComponent.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VanguardWebApp.Models;
+
+namespace VanguardWebApp.Shared
+{
+    public partial class TableComponent
+    {
+        [Parameter]
+        public Boss Boss { get; set; }
+
+        public string FormatSpec(Priority priority)
+        {
+            if (priority == null)
+            {
+                return $"/images/loot/{priority.Spec}_off.png";
+            }
+
+            var formattedSpec = $"/images/loot/{priority.Spec}.png";
+            return formattedSpec;
+        }
+
+        public string CalculatePriorityClass(Priority priority)
+        {
+            if (priority == null)
+            {
+                return "norank";
+            }
+
+            switch (priority.PriorityValue)
+            {
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+
+            }
+
+            return "norank";
+        }
+    }
+}
