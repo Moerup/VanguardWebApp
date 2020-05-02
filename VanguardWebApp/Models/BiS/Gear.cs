aa37d1d28dbed9d33adfbf47170bf7439380df5a
Tobias <tobiasdanbo@gmail.com>
2020-05-02 18:10:47 +0100

    changes to the bis list

--- VanguardWebApp/Models/BiS/Gear.cs
+++ VanguardWebApp/Models/BiS/Gear.cs
@@ -9,6 +9,8 @@ namespace VanguardWebApp.Models.BiS
 {
     public class Gear
     {
+        public string Slot { get; set; }
+
         public string Name { get; set; }
 
         [BsonElement("Id")]
