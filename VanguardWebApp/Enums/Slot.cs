4f819ceb87f2c2ac96987583a6a96cf8b702a500
Nikolaj Mørup <nikolajmorup@gmail.com>
2020-06-13 17:46:21 +0200

    Updated bis list page

--- VanguardWebApp/Enums/Slot.cs
+++ VanguardWebApp/Enums/Slot.cs
@@ -1,15 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace VanguardWebApp.Enums
 {
     public enum Slot
     {
-        Armor,
-        Weapon,
+        Head,
+        Neck,
+        Shoulder,
+        Back,
+        Chest,
+        Wrist,
+        Hands,
+        Waist,
+        Legs,
+        Feet,
         Finger,
-        Trinket
+        Trinket,
+
+        [EnumMember(Value = "Main Hand")]
+        MainHand,
+
+        [EnumMember(Value = "One-hand")]
+        OneHand,
+
+        [EnumMember(Value = "Off Hand")]
+        OffHand,
+
+        [EnumMember(Value = "Held in Off-Hand")]
+        HeldInOffHand,
+
+        [EnumMember(Value = "Two-hand")]
+        TwoHand,
+
+        Ranged
+    }
+
+    public static class Groups
+    {
+        public static bool IsArmor(Slot slot)
+        {
+            switch (slot)
+            {
+                case Slot.Head:
+                case Slot.Neck:
+                case Slot.Shoulder:
+                case Slot.Back:
+                case Slot.Chest:
+                case Slot.Wrist:
+                case Slot.Hands:
+                case Slot.Waist:
+                case Slot.Legs:
+                case Slot.Feet:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsWeapon(Slot slot)
+        {
+            switch (slot)
+            {
+                case Slot.MainHand:
+                case Slot.OneHand:
+                case Slot.OffHand:
+                case Slot.HeldInOffHand:
+                case Slot.TwoHand:
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
