f349645b7c21555b6b5f4ecc51bc38dd3a2b777f
Nikolaj Mørup <nikolajmorup@gmail.com>
2020-07-10 19:24:17 +0200

    RosterSpecs updated

--- VanguardWebApp/Models/Roster/RosterSpec.cs
+++ VanguardWebApp/Models/Roster/RosterSpec.cs
@@ -6,9 +6,26 @@ using VanguardWebApp.Enums;
 
 namespace VanguardWebApp.Models
 {
+    public enum SpecForRoster
+    {
+        Holy,
+        Discipline,
+        Shadow,
+        Bear,
+        Cat,
+        Restoration,
+        Marksmanship,
+        Frost,
+        Fire,
+        Destruction,
+        Combat,
+        Fury,
+        Protection
+    }
+
     public class RosterSpec
     {
-        public Spec Spec { get; set; }
+        public SpecForRoster Spec { get; set; }
 
         public List<Player> Players { get; set; }
     }
